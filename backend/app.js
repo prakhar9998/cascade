5567e92fa9e4f29cbc2642122cfb3fba224ff862
prakhar9998 <prakhar.varshney33@gmail.com>
2020-08-03 21:51:06 +0530

    database connection and configure eslint

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+dotenv.config();
+
+// setting up mongoose connection
+const mongoDB = process.env.DEV_DB_URL;
+mongoose.connect(
+  mongoDB,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => { console.log('connected to db!'); },
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
 app.use(logger('dev'));
 app.use(express.json());
