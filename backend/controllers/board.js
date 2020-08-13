7235b18718535c3d0cced44da68979a73ac121d7
prakhar9998 <prakhar.varshney33@gmail.com>
2020-08-13 21:53:11 +0530

    add get board api

--- backend/controllers/board.js
+++ backend/controllers/board.js
@@ -1,10 +1,8 @@
+const mongoose = require('mongoose');
+
 const asyncHandler = require('../middlewares/async');
 const ErrorResponse = require('../utils/errorResponse');
-// const User = require('../models/User');
-// const Board = require('../models/Board');
-const {
-  createBoardValidation,
-} = require('../validation/boardValidation');
+const { createBoardValidation } = require('../validation/boardValidation');
 const Board = require('../models/Board');
 
 exports.createBoard = asyncHandler(async (req, res, next) => {
@@ -21,7 +19,30 @@ exports.createBoard = asyncHandler(async (req, res, next) => {
   });
   board.members.push(req.user);
 
-  return res
-    .status(201)
-    .json({ success: true, data: { title: board.title } });
+  return res.status(201).json({ success: true, data: { id: board._id } });
+});
+
+exports.getBoard = asyncHandler(async (req, res, next) => {
+  const { boardId } = req.params;
+  if (!boardId) {
+    return next(new ErrorResponse('Resource not specified', 400));
+  }
+
+  try {
+    const board = await Board.findOne({ _id: boardId });
+    console.log('board is', board);
+    const responseData = {
+      id: board._id,
+      title: board.title,
+      description: board.description,
+      members: board.members,
+      creator: board.creator,
+    };
+
+    console.log('response data', responseData);
+
+    return res.status(200).json({ success: true, data: { ...responseData } });
+  } catch (err) {
+    return next(new ErrorResponse('Resource not found'), 404);
+  }
 });
--- backend/routes/board.js
+++ backend/routes/board.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { createBoard } = require('../controllers/board');
+const { createBoard, getBoard } = require('../controllers/board');
 const { protect } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/create', protect, createBoard);
+router.get('/:boardId', protect, getBoard);
 
 module.exports = router;
