dfe6da1e3d5ab09b4afbcfdee2d2fcef1d8f5af1
prakhar9998 <prakhar.varshney33@gmail.com>
2020-09-07 14:44:28 +0530

    board add feature

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,12 +3,17 @@ import styled from "styled-components";
 import axios from "axios";
 import API_URL from "../config/config";
 import AuthService from "../services/authService";
+import BoardService from "../services/boardService";
 
 const Container = styled.div``;
 const BoardContainer = styled.div``;
+const Input = styled.input``;
+const Label = styled.label``;
+const Button = styled.button``;
 
 const Dashboard = () => {
   const [boards, setBoards] = useState([]);
+  const [boardData, setBoardData] = useState({ title: "", description: "" });
 
   useEffect(() => {
     axios
@@ -27,12 +32,47 @@ const Dashboard = () => {
     });
   };
 
+  const handleTitleChange = (e) => {
+    const title = e.target.value;
+    setBoardData((prevState) => ({ ...prevState, title: title }));
+  };
+
+  const handleDescriptionChange = (e) => {
+    const description = e.target.value;
+    setBoardData((prevState) => ({ ...prevState, description: description }));
+  };
+
+  const createBoard = () => {
+    BoardService.createBoard(boardData.title, boardData.description).then(
+      (res) => {
+        console.log("success");
+      }
+    );
+  };
+
   return (
     <Container>
       {boards.map((board) => (
-        <BoardContainer key={board._id}>{board.title}</BoardContainer>
+        <BoardContainer key={board._id}>
+          <h1>Title: {board.title}</h1>
+          <h3>Id: {board._id}</h3>
+        </BoardContainer>
       ))}
-      <button onClick={logout}>logout</button>
+      <Button onClick={logout}>logout</Button>
+      <Label htmlFor="title">Board Title</Label>
+      <Input
+        name="title"
+        value={boardData.title}
+        onChange={handleTitleChange}
+        required
+      />
+      <Label htmlFor="description">Board Description</Label>
+      <Input
+        name="description"
+        value={boardData.description}
+        onChange={handleDescriptionChange}
+      />
+      <Button onClick={createBoard}>Create Board</Button>
     </Container>
   );
 };
--- frontend/src/services/boardService.js
+++ frontend/src/services/boardService.js
@@ -1,6 +1,21 @@
 import axios from "axios";
 import API_URL from "../config/config";
 
+const createBoard = (title, description) => {
+  return axios
+    .post(
+      API_URL + "/api/board/create",
+      { title: title, description: description },
+      { withCredentials: true }
+    )
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      console.log("error occured", err);
+    });
+};
+
 const getBoard = (id) => {
   return axios
     .get(API_URL + `/api/board/${id}`, { withCredentials: true })
@@ -11,4 +26,4 @@ const getBoard = (id) => {
     });
 };
 
-export default { getBoard };
+export default { createBoard, getBoard };
